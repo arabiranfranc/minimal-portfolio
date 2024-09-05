435b930377bb6801e06a0afcbee80d6065d4f4ac
sic <arabiran@prans.com>
2024-09-05 16:21:19 +0800

    change image paths

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,19 +1,19 @@
 const portfolio = [
   {
     title: "Wingspan Game Recorder",
-    imgUrl: "src/assets/wingspan.jpg",
+    imgUrl: "/assets/Wingspan.jpg",
     stack: ["React TS", "Tailwind", "Vite", "Redux"],
     link: "",
   },
   {
     title: "ECOM Shopping Cart",
-    imgUrl: "src/assets/eCommerce-logo.jpg",
+    imgUrl: "/assets/eCommerce-logo.jpg",
     stack: ["React TS", "Bootstrap", "Vite"],
     link: "",
   },
   {
     title: "Developer Portfolio",
-    imgUrl: "src/assets/me.jpg",
+    imgUrl: "/assets/me.jpg",
     stack: ["React TS", "Tailwind", "Vite"],
     link: "",
   },
