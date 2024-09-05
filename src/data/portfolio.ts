e21f3e00cee1df530950d26aa72f7ae32860cf9b
sic <arabiran@prans.com>
2024-09-05 15:56:12 +0800

    update portfolio

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,13 +1,19 @@
 const portfolio = [
   {
-    title: "Developer Portfolio",
-    imgUrl: "",
-    stack: ["React TS", "Tailwind", "Vite"],
+    title: "Wingspan Game Recorder",
+    imgUrl: "src/assets/wingspan.jpg",
+    stack: ["React TS", "Tailwind", "Vite", "Redux"],
+    link: "",
+  },
+  {
+    title: "ECOM Shopping Cart",
+    imgUrl: "src/assets/eCommerce-logo.jpg",
+    stack: ["React TS", "Bootstrap", "Vite"],
     link: "",
   },
   {
     title: "Developer Portfolio",
-    imgUrl: "",
+    imgUrl: "src/assets/me.jpg",
     stack: ["React TS", "Tailwind", "Vite"],
     link: "",
   },
--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -1,15 +1,17 @@
 const timeline = [
   {
     year: "2022",
-    title: "Freelance React Frontend",
-    duration: "1 year",
-    details: "test asjdlkajsdlqwj an.msmdlkwqjeqlj ashdlakhjslqkjwhe q",
+    title: "React Frontend Engineer",
+    duration: "2 years",
+    details:
+      "As a React Front Engineer. My job was to able to apply the approved UI/UX design from figma. Handle response from backend api using Axios and SignalR for the live data. Create Reusable Components.",
   },
   {
     year: "2017",
     title: "Bachelor's of Science in Information Technology",
     duration: "5 years",
-    details: "testlashdlahs lkahsdlkjahsldkha lashdlaskhd laskhdnqwlkehq weq",
+    details:
+      "This program was completed at University of the East, where students engaged in a comprehensive curriculum designed to equip them with a broad range of skills and knowledge in the field of information technology. The coursework included subjects such as programming, database management, networking, and systems analysis.",
   },
 ];
 
